a5be7ea525f1f1c07a2679ce386f62ddd5027220
Manuela <manuelatourinhodeoliveira@gmail.com>
2024-07-01 20:10:44 -0300

    modal and save new user  not working

--- src/main/java/habsida/spring/boot_security/demo/controller/WebAppController.java
+++ src/main/java/habsida/spring/boot_security/demo/controller/WebAppController.java
@@ -5,6 +5,8 @@ import habsida.spring.boot_security.demo.model.User;
 import habsida.spring.boot_security.demo.repository.RoleRepository;
 import habsida.spring.boot_security.demo.repository.UserRepository;
 import habsida.spring.boot_security.demo.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -23,6 +26,8 @@ public class WebAppController {
     private final UserService userService;
     private final RoleRepository roleRepository;
     private final UserRepository userRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(WebAppController.class);
 
     @Autowired
     public WebAppController(UserService userService, RoleRepository roleRepository, UserRepository userRepository) {
@@ -62,7 +67,7 @@ public class WebAppController {
 //        if (user == null) {
 //            session.setAttribute("currentUser", user);
 //            return "redirect:/";
-//.
+//
 //        } else {
 //            model.addAttribute("error", "Invalid email or password");
 //            return "login";
@@ -110,6 +115,7 @@ public class WebAppController {
         model.addAttribute("user", new User());
 
         return "admin/admin-home";
+
     }
 
     @GetMapping("/admin/add")
@@ -118,39 +124,38 @@ public class WebAppController {
         model.addAttribute("loggedUser", loggedUser);
         model.addAttribute("user", new User());
         model.addAttribute("roles", roleRepository.findAll());
+
         return "admin/add-user-form";
 
     }
 
     @PostMapping("/admin/add")
-    public String addUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+    public String addUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @RequestParam("selectedRoles") Set<Role> selectedRoles) {
         if (bindingResult.hasErrors()) {
+            logger.error("Error creating new user {}", user);
             return "admin/add-user-form";
         }
 
-//        Set<Role> roles = roleId.stream()
-//                        .map(roleRepository::findById)
-//                                .filter(Optional::isPresent)
-//                                        .map(Optional::get)
-//                                                .collect(Collectors.toSet());
 
-        Set<Role> roles = user.getRoles();
-        user.setRoles(roles);
+        user.setRoles(selectedRoles);
         userService.saveOrUpdateUser(user);
+        logger.info("new user was created {}", user);
         return "redirect:/admin";
 
     }
 
-    @GetMapping(value = "admin/update/{id}")
-    public String showUserUpdateForm(@PathVariable Long id, Model model) {
-        model.addAttribute("user", userService.findById(id));
-        model.addAttribute("roles", roleRepository.findAll());
-        return "admin/admin-home";
-    }
+//    @GetMapping(value = "admin/update/{id}")
+//    public String showUserUpdateForm(@PathVariable Long id, Model model) {
+//        model.addAttribute("user", userService.findById(id));
+//        model.addAttribute("roles", roleRepository.findAll());
+//        return "admin/admin-home";
+//    }
 
-    @PostMapping("admin/update/{id}")
+    @GetMapping("admin/update/{id}")
+    @ResponseBody
     public String update(@PathVariable("id") Long id, @ModelAttribute("user") User user) {
         userService.saveOrUpdateUser(user);
+        logger.info("user has been updated {}", user);
         return "redirect:/admin";
 
     }
@@ -158,21 +163,10 @@ public class WebAppController {
     @PostMapping(value = "admin/delete/{id}")
     public String delete(@PathVariable("id") long id) {
         userService.removeUserById(id);
+        logger.info("user has been deleted. User id {}", id);
         return "redirect:/admin";
 
     }
 
-    @GetMapping("/admin/list")
-    public String adminListView(Model model) {
-        try {
-            List<User> users = userService.getAllUsers();
-            model.addAttribute("users", users);
-        } catch (Exception e) {
-            model.addAttribute("users", null);
-        }
-
-        return "admin/admin-list-view";
-
-    }
 
 }
